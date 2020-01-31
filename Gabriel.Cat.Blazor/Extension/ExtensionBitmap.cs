bf2438f9e28300c2197e2677912b141b071b90ec
gabriel <tetradog@gmail.com>
2020-01-31 06:01:42 +0100

    subir archivos :)

---
+++ Gabriel.Cat.Blazor/Extension/ExtensionBitmap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Gabriel.Cat.S.Extension;
+namespace Gabriel.Cat.Extension
+{
+    public static class ExtensionBitmap
+    {
+        public static async ValueTask<string> GetUrl(this Bitmap bmp)
+        {
+            return bmp.GetBytes().GetUrl();
+        }
+    }
+}
---
+++ Gabriel.Cat.Blazor/Extension/ExtensionByteArray.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gabriel.Cat.Extension
+{
+   public static class ExtensionByteArray
+    {
+        [inject] IJSRuntime js;
+        public static async ValueTask<string> GetUrl(this byte[] data)
+        {
+            return  js.InvokeAsync<string>("GetUri", data);
+        }
+    }
+}
