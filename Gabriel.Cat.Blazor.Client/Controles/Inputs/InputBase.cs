176542c5939869f1608b129a7bcd7cf93ecdded0
Gabriel Cortés Garcia <tetradog@gmail.com>
2020-08-01 12:59:39 +0000

    Add files via upload

---
+++ Gabriel.Cat.Blazor.Client/Controles/Inputs/InputBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Gabriel.Cat.Blazor.Client.Controles.Inputs
+{
+    public class InputBase<T> : ComponentBase
+    {
+
+        [Parameter]
+        public T Value { get; set; }
+
+    }
+}
