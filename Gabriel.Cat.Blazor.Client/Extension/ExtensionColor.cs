5d58c3c80f4c53781299000a3612529da81dcb65
Gabriel Cortés Garcia <tetradog@gmail.com>
2020-08-01 13:23:16 +0000

    Add files via upload

---
+++ Gabriel.Cat.Blazor.Client/Extension/ExtensionColor.cs
@@ -0,0 +1,16 @@
+﻿using Gabriel.Cat.S.Utilitats;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Gabriel.Cat.S.Extension
+{
+    public static class ExtensionColor
+    {
+        public static string ToHtml(this Color color)
+        {//rgba
+            return (Hex)new byte[] {color.R,color.G,color.B,color.A };
+        }
+    }
+}
